0325bd9804bc4debc2980635e8586ad772d89c40
Naresh <programography@gmail.com>
2023-10-16 22:39:05 +0530

    nested if else

---
+++ lacture-19-nested-if-else.py
@@ -0,0 +1,25 @@
+            
+            # if    elif    else
+            
+# Nested if else : 
+
+
+age = int(input("Enter your age : "))
+
+# 17 >= 18
+if age >= 18:
+    if age == 18:
+
+        print("welcom this is your first voat")
+    
+    elif age > 19:
+        print("i am more then 19")
+    
+    else:
+        print("ok")
+
+elif age == 19:
+    print("i am 19")
+
+else:
+    print("i am else part")
