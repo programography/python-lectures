4cb7ef6c1c9321b8cd0f7e3b8de3f4a4641e8301
Naresh <programography@gmail.com>
2023-10-16 23:11:13 +0530

    for loop.py

---
+++ lecture-20-for-loop.py
@@ -0,0 +1,42 @@
+# Loops : used for ittrations ( repetation )
+
+# 1) for loop
+# 2) while loop
+
+
+# 1) for loop : use when you have a sequence
+
+# 
+# for variable in sequence:
+#     # code of block
+
+# zx = [12, 10, 89, 78, 4, 5, 6,7, 10]
+
+
+# for i in zx:
+#     print(i)
+#     print("hello wolrld")
+#     print("the value of i is : ", i)
+#     print("okkkkk")
+
+
+# list, tuple, set, dict, string
+
+# a = "this is ok"
+
+# for x in a:
+#     print(x)
+
+
+# a = (1, 5, 6, 7, 3, 4, 6, 1, 10)
+
+
+# for j in a:
+#     print(j)
+
+
+
+# range()
+
+
+
