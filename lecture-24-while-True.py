711ffdf39abf2628690ae684ed73b9399c00bc7a
Naresh <programography@gmail.com>
2023-10-17 23:08:56 +0530

    while True

---
+++ lecture-24-while-True.py
@@ -0,0 +1,29 @@
+# while condation:
+
+
+# while True:
+#     if conadtion
+
+
+while True:
+    choice = input("Enter q for quit and s for start : ")
+    
+    if choice == "q":
+        break
+
+    elif choice == "s":
+        print("hello this is me..!")
+    
+    else:
+        print("Enter somthing valid")
+
+
+
+
+
+
+
+
+
+
+
