b37ba39e6fda6548660d35264cd253e2aaaf3eef
Naresh <programography@gmail.com>
2023-10-17 22:58:43 +0530

    while loop

---
+++ lecture-23-while-loop.py
@@ -0,0 +1,48 @@
+# loop:
+#     1) for loop :    sequence
+#     2) while loop  :  condation
+
+
+
+# age = 12
+
+#     #  12 < 100
+# while age < 100:
+#     print("helloo", age)
+
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# a = 1
+#     #   2 <= 10
+# while a <= 10:
+#     print(a)
+#     a = a + 1
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# .
+# .
+# .
+# .
+# 2 x 10 = 20
+
+
+
+# a = 1
+
+# while a < 11:
+#     print("2 x", a, "=", 2*a)
+#     a = a + 1
+
+
+
+
+
+
+
+# while True :
