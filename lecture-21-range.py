94da16e7b2ddafda9f0e08e21b17f1c41fc229e3
Naresh <programography@gmail.com>
2023-10-17 22:21:48 +0530

    range funcation

---
+++ lecture-21-range.py
@@ -0,0 +1,27 @@
+# range() => 
+
+# range(start, end, increment)
+
+# default : 
+# start = 0
+# end = n - 1
+# increment = 1
+
+# for i in range(8):
+#     print(i)
+
+# for i in range(1, 11):
+#     print("3 x", i, "=", 3*i)
+
+
+# 3 x 1 = 3
+# 3 x 2 = 6
+# 3 x 3 = 9
+# 3 x 4 = 12
+# .
+# .
+# .
+# ..
+# .
+# .
+# 3 x 10 = 30
