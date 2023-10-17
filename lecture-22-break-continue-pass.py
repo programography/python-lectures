6139f5749b36a30fb95cfdf012a2467bb5c795db
Naresh <programography@gmail.com>
2023-10-17 22:44:52 +0530

    python loops control statments

---
+++ lecture-22-break-continue-pass.py
@@ -0,0 +1,63 @@
+#             Ptyhon For Loop
+
+# control statements:
+#     1) break : to break loop
+#     2) continue : 
+#     3) pass :
+
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# lst = ["hlo", "ok", "12", 100, 89, 10]
+
+# for i in lst:
+#     print(i)
+
+# for i in range(1, 10):
+#     print(i)
+    
+#     if i == 6:
+#         break
+    
+
+
+# lst = [12, 34, 4,5, 10, 10, 90, 78, 3, 4, 5]
+
+# for i in lst:
+#     if i == 78:
+#         break
+#     print(i)
+
+
+
+
+# continue : 
+
+
+# for i in range(1, 11):
+#     if i == 6:
+#         continue
+#     print(i)
+
+
+# lst = [1, 4, 7, 2, 4, 5, 7, 4, 10, 89, 7]
+
+
+# for i in lst:
+#     if i == 7:
+#         continue
+#     print(i)
+
+
+
+
+# pass : 
+
+
+for i in range(12):
+    pass
+
+
+print("helloo")
