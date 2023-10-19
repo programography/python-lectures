2623668ac18392ec402ba329f6d9ed1fd917aa2c
Naresh <programography@gmail.com>
2023-10-19 07:23:56 +0530

    funcations.

---
+++ lecture-25-intro-python-funcation.py
@@ -0,0 +1,38 @@
+#               Python Functions
+# Functions in Python are blocks of reusable
+# code that perform specific tasks or calculations. 
+
+# xyz()
+
+# types of funcation:
+#     1) bulit funcations:
+#         print()
+#         input()
+#         type()
+#         len()
+#         count()
+        
+#     2) user define funcation
+
+
+# funcation are used for code optimization
+# DRY = do not repete yourself
+a = 10
+b = 20
+c = a + b
+print(c)
+
+d = 100
+e = 300
+f =  d + e
+print(f)
+
+g = 34
+h = 8
+i = g + h
+print(i)
+
+
+
+
+
---
+++ lecture-26-define-a-funcation.py
@@ -0,0 +1,48 @@
+# Funcations are use for code optimization.
+
+# define
+
+# decleartion of a funcation
+# def xyz():
+#     print("Heloo world")
+#     print("hey, how are youo")
+    
+# call a funcation
+
+# xyz()
+
+# xyz()
+
+# xyz()
+
+# xyz()
+
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# d = 100
+# e = 300
+# f =  d + e
+# print(f)
+
+# g = 34
+# h = 8
+# i = g + h
+# print(i)
+
+
+
+# perameters
+
+# arguments
+
+
+
+
+
+
+
+
