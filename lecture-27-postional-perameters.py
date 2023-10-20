88fe43dbf83054d59345d699aadb1e8fa8a8334e
Naresh <programography@gmail.com>
2023-10-20 07:35:30 +0530

    permeters

---
+++ lecture-27-postional-perameters.py
@@ -0,0 +1,37 @@
+#                   Funcation 
+#              Parameters,Arguments
+    
+# Type of Parameters:        
+#     1) Positional Parameters
+#     2) Keyword Parameters
+#     3) Default Parameters
+#     4) Variable-Length Parameters
+#         a) *args
+#         b) **kwargs
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# f = 67
+# g = 89
+# h = f + g
+# print(h)
+
+
+# Parameters : are pass in the perameter while we decler a funcation
+#  1) Positional Parameters   
+def sumthis(a, b):
+    print("vales : ", a)
+    print("vale : ", b)
+    c = a + b
+    print(c)
+
+
+# argments : are the values, that pass while we call a funcation
+sumthis(10, 20)
+
+sumthis(100, 200)
+
+
---
+++ lecture-28-keyword-or-default-preameters.py
@@ -0,0 +1,47 @@
+#                   Funcation 
+#              Parameters,Arguments
+    
+# Type of Parameters:        
+#     1) Positional Parameters
+#     2) Keyword Parameters
+#     3) Default Parameters
+#     4) Variable-Length Parameters
+#         a) *args
+#         b) **kwargs
+
+
+#  2) Keyword Parameters
+
+
+# def saveadta(x, y):
+#     print("Username is : ", x)
+#     print("Age is : ", y)
+
+
+# # saveadta("Kriss", 20)
+
+# saveadta(y = 20, x = "moris")
+
+
+
+# 3) Default Parameters
+
+
+def sumthis(x = 0, y = 0, z = 0):
+    zx = x + y + z
+    print("this sum is : ", zx)
+
+
+sumthis(4, 3, 2)
+
+
+
+
+
+
+
+
+
+
+
+
