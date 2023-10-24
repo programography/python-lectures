b054710186a985ba66f09d149ed9c790dd465819
Naresh <programography@gmail.com>
2023-10-24 07:27:34 +0530

    lembda funcation

---
+++ lecture-30-return-statment.py
@@ -0,0 +1,40 @@
+#                 Return Statments
+                
+# Return in functions, is like sending a value back to the place in your code where you called the function.
+
+
+# def xyz(a, b):
+#     c = a + b
+#     print(c)
+    
+# xyz(12, 10)
+
+# print(c)
+# d = c + 12
+
+# print(d)
+
+# def sumthis(a, b):
+#     c = a + b
+#     return c
+
+# x = sumthis(100, 200)
+
+
+# newsum = x + 1000
+
+# print(newsum)
+
+
+def savedaa():
+    name = "kriss moris"
+    return name
+
+
+x = savedaa()
+print(x)
+
+
+
+
+
---
+++ lecture-31.,embda funcation.py
@@ -0,0 +1,32 @@
+#              lambda functions
+             
+# anonymous function is a function without a name. Anonymous functions are also called lambda functions. 
+#  They are often used when you need a simple, small, and short function for a specific task.
+ 
+#  def xyz()
+
+# syntex:
+    
+# lambda arguments : expression
+
+
+# x = lambda a, b : a + b
+
+# print(x(100, 200))
+
+# print(x(1, 2))
+
+# def xyz(a, b):
+#     c = a - b
+#     print(c)
+
+# xyz(10, 20)
+
+
+xyz = lambda a : a ** 2
+
+print(xyz(3))
+
+
+
+
