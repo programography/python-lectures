311dc8750cfce829f675e28c49cdd5971c73c108
programography <programography@gmail.com>
2023-10-31 22:55:59 +0530

    recuersion

---
+++ lecture-38-recursion.py
@@ -0,0 +1,37 @@
+#         Recursion (Recursive functions )
+        
+Recursion is a programming technique in which a function calls itself to solve a problem.
+
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:   
+#         return n * factorial(n-1)
+#         #   4 x factorial(3)
+#         #   4 x 3 x factorial(2)
+#         #   4 x 3 x 2 x fa
+
+
+# x = factorial(5)
+# print(x)
+
+
+# 4! = 4 x 3 x 2 x 1 = 24
+
+# 5! = 5 x 4 x 3 x 2 x 1
+
+
+
+
+
+
+
+# 0, 1, 1, 2, 3, 5, 8, 13, ......
+
+
+
+
+
+
+
