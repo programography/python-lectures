b42f96d9e3ce7b858d0da8108d05b1f302752f04
programography <programography@gmail.com>
2023-10-31 23:37:38 +0530

    compoerhesion

---
+++ lecture-39-comperhension.py
@@ -0,0 +1,40 @@
+#               Comprehensions
+#           (Use to generate data)
+
+# 1) List comprehensions
+# 2) Dictionary comprehensions
+# 3) Set comprehensions 
+
+[experssion loop condation]
+
+
+# lst = [i+10 for i in range(1, 11) if i % 2 != 0]
+
+# print(lst)
+
+
+# 2) Dictionary comprehension
+
+# {key : value}
+
+# [key expression : value experssion loop condation]
+
+# dic = {x ** 2 : x + 3 for x in range(1, 9)}
+
+# print(dic)
+
+# 3) Set comprehensions 
+
+# {experersion loop condation}
+
+
+zx = {i for i in range(11, 20) if i % 2 == 0}
+
+print(zx)
+
+
+
+
+
+
+
