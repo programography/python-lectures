9a30318cfb1cbe3dcfdec35ee5b113905be01e04
programography <programography@gmail.com>
2023-11-01 22:13:00 +0530

    start opps

---
+++ lecture-40-start-oops.py
@@ -0,0 +1,36 @@
+                   Python
+        Object-Oriented Programming (OOP)
+        
+1) Class
+2) objcets
+3) self
+4) constructer
+5) Attributes
+6) Methods
+7) Inharitance:
+    1) Single Inharitance
+    2) Multipal inharitance
+    3) Multilevel Inhariance
+    4) Hirical inharitance
+    5) Hibrid inhariance
+
+8) Abstraction:
+    1) Abstract Methods
+    2) Abstract Class
+    3) INterfaces
+
+9) Encapsulation:
+    Access spacifiers:
+        1) Public Mambers
+        2) Protected mambers
+        3) Private mambers
+
+10) Polymorphisum:
+    1) Compile Time polymorphisum
+        1) method overloading
+        2) Operator overloading
+    2) Run Time polymorphisum
+        1) Method overriding
+     Duck Typing
+    
+    
