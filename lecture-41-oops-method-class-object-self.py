ab411aa4a47fd8a3f4d5513ddba7daadefff85cc
programography <programography@gmail.com>
2023-11-01 23:08:05 +0530

    opps part-2

---
+++ lecture-41-oops-method-class-object-self.py
@@ -0,0 +1,29 @@
+#                   Python
+#         Object-Oriented Programming (OOP)
+        
+# 1) Class : Class is a blueprint of an object. 
+# 2) objcets : object is a instance of a class.
+# 3) self : is used for refrencing
+# 4) Methods
+
+class myclass:
+    def car(self):
+        print("This is a car")
+    
+    def bus(self):
+        print("this is bus..")
+    
+
+obj = myclass()  
+    
+obj.car()   
+ 
+obj.bus()  
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ lecture-42-opps-method-self-class-obj-2.py
@@ -0,0 +1,32 @@
+#                   Python
+#         Object-Oriented Programming (OOP)
+        
+# 1) Class : Class is a blueprint of an object. 
+# 2) objcets : object is a instance of a class.
+# 3) self  :   is used for refrencing
+# 4) Methods
+
+class mycls:
+    def mthd1(self):
+        print("This is a method 1")
+    
+    def mthd2(self):
+        print("this is method 2")
+
+    def mthd3(self):
+        print("this is method 3")
+
+x = mycls()
+
+x.mthd2()
+x.mthd3()
+
+xyz = mycls()
+
+xyz.mthd2()
+
+
+
+
+
+
