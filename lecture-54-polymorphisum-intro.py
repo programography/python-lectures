0df3517190b74c3f1932149296e0dccb6efd07ea
programography <programography@gmail.com>
2023-11-16 22:18:05 +0530

    ploymmorphisum'

--- lecture-54-polymorphisum-intro.py
+++ lecture-54-polymorphisum-intro.py
@@ -4,20 +4,21 @@ Polymorphism:
     Poly : Many
     Morphism : froms
 
-when one thing have multiple froms.
+Polymorphism : when one thing have multiple froms.
 
 
 Polymorphism allows you to use a single interface (method name) to represent different behaviors (implementations) based on the context or the type of that object involved.
 
-TYpes of Polymorphism:
+
+Types of Polymorphism:
     1) Compile time Polymorphism
-        1) Method overloading
-        2) Operator overloading
-        
-    2) Run time Polymorphism
-        1) Method overloading
+        1) Operator Overlaing
+        2) Method overloading
     
-    Duck Typeing
+    2) Run time Polymorphism
+        1) Method overriding
+    
+    Duck Typing
     
     
     
