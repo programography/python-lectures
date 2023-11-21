e16ec3c8966b6a2597babcf218123192a8f1e274
programography <147963679+programography@users.noreply.github.com>
2023-11-21 17:21:42 +0530

    Update lecture-58-duck-typing.py

--- lecture-58-duck-typing.py
+++ lecture-58-duck-typing.py
@@ -1,41 +1,47 @@
-                        Polymorphism
-Types of Polymorphism:
-    1) Compile time Polymorphism
-        1) Operator Overloading
-        2) Method overloading
+#  Polymorphism
+# Types of Polymorphism:
+#     1) Compile time Polymorphism
+#         1) Operator Overloading
+#         2) Method overloading
     
-    2) Run time Polymorphism
-        1) Method Overriding
+#     2) Run time Polymorphism
+#         1) Method Overriding
             
-                Duck Typing In Python OOP's
+                
+#                 Duck Typing In Python OOP's
                     
-Duck typing is a programming concept in Python that focuses on an object's behavior rather than its type or class. The term "duck typing"' comes from the saying, "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck." 
+# Duck typing is a programming concept in Python that focuses on an object's behavior rather than its type or class. The term "duck typing"' comes from the saying, "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck." 
 
 
 class dog:
     def speak(self):
-        print("woof")
+        print("wooof")
+    
+    def walk(self):
+        print("slow and fast")
 
 class cat:
     def speak(self):
-        print("mwaww")
+        print("meaw")
+    
+    def walk(self):
+        print("cat walk")
 
 class duck:
     def speak(self):
         print("quack")
+    
+    def walk(self):
+        print("thapck thpack")
 
 obj1 = dog()
 obj2 = cat()
 obj3 = duck()
 
-def animal_sound(x):
+
+def animal_behave(x):
     x.speak()
-
-animal_sound(obj3)
-
+    x.walk()
 
 
-
-
-
-
+animal_behave(obj3)
