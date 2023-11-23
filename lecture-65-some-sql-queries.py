229660f7849ce74083a9eccdd10de92fd4be9e0f
programography <147963679+programography@users.noreply.github.com>
2023-11-23 23:28:33 +0530

    Create lecture-65-some-sql-queries.py

---
+++ lecture-65-some-sql-queries.py
@@ -0,0 +1,12 @@
+show databases;
+
+create database programography;
+
+use programography;
+
+
+create table ContactUs(Name varchar(50), Email varchar(50), message text);
+
+show tables;
+
+select * from ContactUs;
